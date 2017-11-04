0419c40080682bbcc70fd2f310c2e599812fd712
shanirajkali <shanirajkali@gmail.com>
2017-11-04 07:12:30 +0530

    www.reuters.com complete industry group details fetching crawler source CODE

---
+++ src/main/java/kali/web_crawlers/App.java
@@ -0,0 +1,13 @@
+package kali.web_crawlers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
